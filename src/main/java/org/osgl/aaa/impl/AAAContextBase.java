704e972053d45127c169335a1a81a69a46ff3c64
Gelin Luo <greenlaw110@gmail.com>
2015-02-26 13:16:22 +1100

    Remove unnecessary thread local usage in AAAContextBase

--- src/main/java/org/osgl/aaa/impl/AAAContextBase.java
+++ src/main/java/org/osgl/aaa/impl/AAAContextBase.java
@@ -3,36 +3,31 @@ package org.osgl.aaa.impl;
 import org.osgl.aaa.AAAContext;
 import org.osgl.aaa.Principal;
 
-/**
- * Created by luog on 13/01/14.
- */
 public abstract class AAAContextBase implements AAAContext {
 
-    private static final ThreadLocal<Principal> current = new ThreadLocal<Principal>();
-    private static final ThreadLocal<Object> target = new ThreadLocal<Object>();
+    private Principal principal;
+    private Object guarded;
 
 
     @Override
     public void setCurrentPrincipal(Principal user) {
-        if (null == user) current.remove();
-        else current.set(user);
+        principal = user;
     }
 
     @Override
     public Principal getCurrentPrincipal() {
-        return current.get();
+        return principal;
     }
 
     @Override
     public Object setGuardedTarget(Object obj) {
-        Object prev = target.get();
-        if (null == obj) target.remove();
-        else target.set(obj);
+        Object prev = guarded;
+        guarded = obj;
         return prev;
     }
 
     @Override
     public Object getGuardedTarget() {
-        return target.get();
+        return guarded;
     }
 }
