7b1de2e024a465db2ebcf0a0e745e08e0a8feccd
Gelin Luo <greenlaw110@gmail.com>
2016-01-07 22:35:33 +1100

    gu

--- src/main/java/org/osgl/aaa/Auditor.java
+++ src/main/java/org/osgl/aaa/Auditor.java
@@ -18,7 +18,7 @@ public interface Auditor {
      * @param success    if the access is granted or not
      * @param message    any additional message
      */
-    void audit(Object target, String principal, String permission, String privilege, boolean success, String message);
+    void audit(Object target, Principal principal, String permission, String privilege, boolean success, String message);
 
     /**
      * It is recommended that the object which can be a {@code target} of auditing
