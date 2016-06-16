d0709e45f36510ba4b1bef0d0179c80c452b5c84
Gelin Luo <greenlaw110@gmail.com>
2016-06-16 13:51:07 +1000

    remove debug log

--- src/main/java/org/osgl/aaa/impl/SimpleAuthorizationService.java
+++ src/main/java/org/osgl/aaa/impl/SimpleAuthorizationService.java
@@ -38,7 +38,6 @@ public class SimpleAuthorizationService implements AuthorizationService {
     @Override
     public Collection<Permission> getAllPermissions(Principal principal, AAAContext context) {
         C.List<Permission> perms = C.newList(getPermissions(principal, context)).lazy();
-        logger.info("perms: %s", perms);
         C.list(getRoles(principal, context)).accept(Role.F.PERMISSION_GETTER.andThen(C.F.addAllTo(perms)));
         Set<Permission> retVal = C.newSet();
         for (Permission p : perms) {
@@ -46,7 +45,6 @@ public class SimpleAuthorizationService implements AuthorizationService {
                 logger.warn(new RuntimeException(), "Null permission found on principal %s", principal.getName());
                 continue;
             }
-            logger.info("collect implied permission on: %s", p.getName());
             collectPermission(retVal, p);
         }
         return retVal;
