96fbb9d86d725da636c97d68f868068351b1dff3
Gelin Luo <greenlaw110@gmail.com>
2016-09-18 18:48:25 +1000

    add AuthenticationService.authenticate(String, char[]) method

--- src/main/java/org/osgl/aaa/AuthenticationService.java
+++ src/main/java/org/osgl/aaa/AuthenticationService.java
@@ -1,7 +1,9 @@
 package org.osgl.aaa;
 
 /**
- * Created by luog on 8/01/14.
+ * An `AuthenticationService` implementation shall provide the logic to
+ * authenticate a {@link Principal} with the supplied `username` and
+ * `password`
  */
 public interface AuthenticationService {
     /**
@@ -14,4 +16,15 @@ public interface AuthenticationService {
      * @return the principal matches the username and password
      */
     Principal authenticate(String username, String password);
+
+    /**
+     * Authenticate using username and password. Returns a {@link org.osgl.aaa.Principal} if
+     * the username and password can be authenticated. or {@code null} if no principal found
+     * matching the username and password combination.
+     *
+     * @param username the name of the principal
+     * @param password the password
+     * @return the princpal matches the username and password
+     */
+    Principal authenticate(String username, char[] password);
 }
